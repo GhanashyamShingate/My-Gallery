54e896cdb03e1c88afbdf65e619bf182ef86c5e1
GhanashyamShingate <g_shingate@fanshaweonline.ca>
2023-07-05 17:43:44 -0400

    Created Navigation bar and Image gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+//import "./Components/Navbar";
+import Navbar from "./Components/Navbar";
+import Card from "./Components/Card";
+import { useState } from "react";
+//import React from "react";
+const photos = [
+  "https://picsum.photos/id/1001/200/200",
+  "https://picsum.photos/id/1002/200/200",
+  "https://picsum.photos/id/1003/200/200",
+  "https://picsum.photos/id/1004/200/200",
+  "https://picsum.photos/id/1005/200/200",
+  "https://picsum.photos/id/1006/200/200",
+  "https://picsum.photos/id/1010/200/200",
+  "https://picsum.photos/id/1008/200/200",
+  "https://picsum.photos/id/1009/200/200",
+];
 
 function App() {
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, collapse] = useState(false);
+
+  const toggle = () => {
+    collapse(!isCollapsed);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Navbar />
+      <div class="container text-center mt-5">
+        <button
+          className="btn btn-warning mx-3"
+          onClick={() =>
+            setItems(["https://picsum.photos/id/1009/200/200", ...items])
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          +Add
+        </button>
+        <button className="btn btn-success " onClick={toggle}>
+          collapse
+        </button>
+        <h1>Gallery</h1>
+        <div className="row">
+          {items.map((photo, index) => (
+            <Card key={index} src={photo} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
-
 export default App;
